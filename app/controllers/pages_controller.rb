60815e8e4334df011341363d92f380d02357bc0c
sahaymihir <mihir.sahay0123@hotmail.com>
2025-03-28 01:25:13 +0530

    Make ruby embedded HTML instead of static HTML and move css to app/assests/stylesheets

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def admin_login
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  # Set root to the index page
-  root to: redirect('/index.html')
-
-  # Admin Login Page - Make Sure It Doesn't Auto-Redirect
-  get '/admin', to: redirect('/admin_login.html')
-
-  # Handle login request
-  post '/login', to: 'sessions#create'
+  root "pages#index"
+  get "admin_login", to: "pages#admin_login"
 end
