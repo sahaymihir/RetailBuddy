7a4e6017b6e116e99d7ed358873880b69e71d1cb
sahaymihir <mihir.sahay0123@hotmail.com>
2025-03-28 20:46:33 +0000

    Testig something

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "6140-135-235-227-20.ngrok-free.app"
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
